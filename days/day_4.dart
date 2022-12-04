4a417fa93906136a6bc83bcabe79cfcf5bc38590
MystereJack <m.rosset@hotmail.com>
2022-12-04 22:19:35 +0100

    use "for collection"

--- days/day_4.dart
+++ days/day_4.dart
@@ -3,8 +3,7 @@ import 'dart:io';
 import 'package:collection/collection.dart';
 import 'package:tuple/tuple.dart';
 
-typedef Section = Tuple2<String, String>;
-typedef Assignment = Tuple2<int, int>;
+typedef Section = Tuple2<Set<int>, Set<int>>;
 
 void main(List<String> arguments) {
   int solution1 = File('inputs/day_4.txt')
@@ -12,7 +11,6 @@ void main(List<String> arguments) {
       .split('\n')
       .where((line) => line.isNotEmpty)
       .map(_createSection)
-      .map(_createAssignment)
       .map(_calculateContains)
       .sum;
 
@@ -21,7 +19,6 @@ void main(List<String> arguments) {
       .split('\n')
       .where((line) => line.isNotEmpty)
       .map(_createSection)
-      .map(_createAssignment)
       .map(_calculateOverlap)
       .sum;
 
@@ -30,58 +27,27 @@ void main(List<String> arguments) {
 }
 
 Section _createSection(String line) {
-  List<String> split = line.split(',');
-  return Tuple2(split[0], split[1]);
+  List<String> section = line.split(',');
+  List<int> left = section[0].split('-').map((e) => int.parse(e)).toList();
+  List<int> right = section[1].split('-').map((e) => int.parse(e)).toList();
+  return Tuple2(left[0].upTo(left[1]), right[0].upTo(right[1]));
 }
 
-Iterable<Assignment> _createAssignment(Section section) sync* {
-  List<String> split = section.item1.split('-');
-  yield Tuple2(int.parse(split[0]), int.parse(split[1]));
-  split = section.item2.split('-');
-  yield Tuple2(int.parse(split[0]), int.parse(split[1]));
+int _calculateContains(Section section) {
+  if (section.item1.difference(section.item2).isEmpty ||
+      section.item2.difference(section.item1).isEmpty) {
+    return 1;
+  }
+  return 0;
 }
 
-int _calculateContains(Iterable<Assignment> assignments) {
-  if (assignments.elementAt(0).contains(assignments.elementAt(1))) {
+int _calculateOverlap(Section section) {
+  if (section.item1.intersection(section.item2).isNotEmpty) {
     return 1;
-  } else if (assignments.elementAt(1).contains(assignments.elementAt(0))) {
-    return 1;
-  } else {
-    return 0;
   }
-}
-
-int _calculateOverlap(Iterable<Assignment> assignments) {
-  if (assignments.elementAt(0).overlap(assignments.elementAt(1))) {
-    return 1;
-  } else if (assignments.elementAt(1).overlap(assignments.elementAt(0))) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-extension on Assignment {
-  bool contains(Assignment assignment) {
-    if (item1 <= assignment.item1 && item2 >= assignment.item2) {
-      return true;
-    }
-
-    return false;
-  }
-
-  bool overlap(Assignment assignment) {
-    if (item1.isBetween(assignment.item1, assignment.item2) ||
-        item2.isBetween(assignment.item1, assignment.item2)) {
-      return true;
-    }
-
-    return false;
-  }
+  return 0;
 }
 
 extension on int {
-  bool isBetween(int first, int last) {
-    return this >= first && this <= last;
-  }
+  Set<int> upTo(int max) => {for (int i = this; i <= max; i++) i};
 }
