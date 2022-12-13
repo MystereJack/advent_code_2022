46c2aaa62106834d08db987c32ae5b9a71081620
Matthieu Rosset <matthieu.rosset@unifr.ch>
2022-12-13 12:57:05 +0100

    improvements

--- days/day_13.dart
+++ days/day_13.dart
@@ -1,71 +1,65 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math' as math;
 
+import 'package:quiver/iterables.dart';
 import 'package:tuple/tuple.dart';
 
 void main(List<String> arguments) {
-  final lines = File('inputs/day_13.txt')
-      .readAsStringSync()
-      .split('\n')
-      .where((el) => el.isNotEmpty)
-      .toList();
-
-  List<Tuple2<dynamic, dynamic>> pairs = [];
-  String? firstItem;
-
-  for (String line in lines) {
-    if (firstItem != null) {
-      pairs.add(Tuple2(jsonDecode(firstItem), jsonDecode(line)));
-      firstItem = null;
-    } else {
-      firstItem = line;
-    }
-  }
-
-  int solution1 = 0;
-  for (int i = 0; i < pairs.length; i++) {
-    print('START ${i + 1}');
-    try {
-      _isRightOrder(pairs[i].item1, pairs[i].item2);
-    } on OKException {
-      solution1 += (i + 1);
-    } catch (e) {
-      // Nothing to do here
-    }
-  }
+  int solution1 = partition(
+          File('inputs/day_13.txt')
+              .readAsStringSync()
+              .split('\n')
+              .where((el) => el.isNotEmpty)
+              .toList(),
+          2)
+      .map(_createTuple)
+      .fold(Tuple2(1, 0), _solution1)
+      .item2;
 
   print('1: $solution1');
-  print('2: ');
+}
+
+Tuple2<dynamic, dynamic> _createTuple(List<String> lines) {
+  return Tuple2(jsonDecode(lines[0]), jsonDecode(lines[1]));
+}
+
+Tuple2<int, int> _solution1(Tuple2<int, int> previous, Tuple2<dynamic, dynamic> data) {
+  int total = previous.item2;
+  try {
+    _processData(data.item1, data.item2);
+  } on OKException {
+    total += previous.item1;
+  } catch (e) {
+    // Nothing to do here
+  }
+
+  return Tuple2(previous.item1 + 1, total);
 }
 
 class OKException implements Exception {}
 
-void _isRightOrder(List<dynamic> left, List<dynamic> right) {
-  for (int i = 0; i < (right.length > left.length ? right.length : left.length); i++) {
+void _processData(List<dynamic> left, List<dynamic> right) {
+  for (int i = 0; i < math.max(left.length, right.length); i++) {
     if (i >= right.length) {
-      print('KO : RIGHT RAN OUT OF ITEMS');
       throw Exception();
     } else if (i >= left.length) {
-      print('OK : LEFT RAN OUT OF ITEMS');
       throw OKException();
     } else {
       dynamic leftItem = left[i];
       dynamic rightItem = right[i];
 
-      if (leftItem is List && rightItem is List) {
-        _isRightOrder(leftItem, rightItem);
-      } else if (leftItem is List && rightItem is int) {
-        _isRightOrder(leftItem, [rightItem]);
-      } else if (leftItem is int && rightItem is List) {
-        _isRightOrder([leftItem], rightItem);
-      } else {
+      if (leftItem is int && rightItem is int) {
         if (leftItem > rightItem) {
-          print('KO : $leftItem > $rightItem');
           throw Exception();
         } else if (leftItem < rightItem) {
-          print('OK $leftItem < $rightItem');
           throw OKException();
         }
+      } else {
+        _processData(
+          leftItem is int ? [leftItem] : leftItem,
+          rightItem is int ? [rightItem] : rightItem,
+        );
       }
     }
   }
